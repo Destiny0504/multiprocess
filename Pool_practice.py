e57f25a33e115bb29352631e8f4abd29aac2f304
Destiny0504 <105703044@nccu.edu.tw>
2021-09-03 16:32:43 +0800

    practice Pool

--- Pool_practice.py
+++ Pool_practice.py
@@ -7,7 +7,9 @@ from process_b import printing as pb
 
 if __name__ == "__main__":
     test_data = 20
-    with Pool(processes=3) as pool: # the number after the word "processes" can assign how many process do you need
+    with Pool(processes=3) as pool: 
+        # the number after the word "processes" can assign how many process do you need (need to be bigger than 1)
+        
         res = pool.apply_async(pa, (test_data,))      # runs in *only* one process
         test_data = res.get(timeout=0.1)
         print(res.get(timeout=1))      
@@ -24,6 +26,5 @@ if __name__ == "__main__":
         res = pool.apply_async(pa, (test_data,))
         test_data = res.get(timeout=0.1)
 
-        print('==================')
-        print(test_data)
+        print(test_data) # output = 25
 
